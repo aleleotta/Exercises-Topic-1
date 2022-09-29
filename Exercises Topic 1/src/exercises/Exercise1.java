a74475892e5bafb0ef0879125127605f55a68090
aleleotta <aleleotta@106-12.info2>
2022-09-29 13:44:34 +0200

    Exercise 1

---
+++ Exercises Topic 1/src/exercises/Exercise1.java
@@ -0,0 +1,13 @@
+package exercises;
+import java.util.*;
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		double number;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Type in a number: ");
+		number = sc.nextDouble();
+		
+		System.out.println("The following number has been converted to an integer: ");
+	}
+}
