118122ca46fb4e457019e1405a8659dd3f295cce
aleleotta <aleleotta@106-12.info2>
2022-09-29 14:35:16 +0200

    Work in progress!

--- Exercises Topic 1/src/exercises/Exercise2.java
+++ Exercises Topic 1/src/exercises/Exercise2.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 public class Exercise2 {
 	public static void main(String[] args) {
+		int number;
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Input a number: ");
 	}
 }
