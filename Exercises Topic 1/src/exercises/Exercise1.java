e8cbadcee115f7248e5bd89bfa89c3dd9c747508
aleleotta <aleleotta@106-12.info2>
2022-09-29 14:24:33 +0200

    Exercise 1 completed

--- Exercises Topic 1/src/exercises/Exercise1.java
+++ Exercises Topic 1/src/exercises/Exercise1.java
@@ -3,11 +3,14 @@ import java.util.*;
 
 public class Exercise1 {
 	public static void main(String[] args) {
-		double number;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Type in a number: ");
-		number = sc.nextDouble();
-		number = (int) (number*10.0)/10.0;
-		System.out.println("The following number has been converted to an integer: " + number);
+		double number; //Declaration of variables
+		Scanner sc = new Scanner(System.in); //Declaration of scanner
+		//Asking the user for a decimal number.
+		System.out.println("Type in a decimal number: "); //The program asks the user to type in a number.
+		number = sc.nextDouble(); //The user types in the number.
+		number += 0.5; //Equivalent function to Math.round
+		System.out.println("The following number has been converted to an integer: " + (int) number);
+		//Number has been converted to a whole number and the program gives output to the user.
+		sc.close(); //Closing scanner
 	}
 }
