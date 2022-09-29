357ef85a9b5d53e0b55ae965d07c93c298395252
aleleotta <aleleotta@106-12.info2>
2022-09-29 14:40:51 +0200

    Work in progress!

--- Exercises Topic 1/src/exercises/Exercise2.java
+++ Exercises Topic 1/src/exercises/Exercise2.java
@@ -4,7 +4,12 @@ import java.util.*;
 public class Exercise2 {
 	public static void main(String[] args) {
 		int number;
+		int remainder;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Input a number: ");
+		System.out.println("Type in a number that is not a multiple of 7: ");
+		number = sc.nextInt();
+		remainder = 0; //Not finished!
+		System.out.println("The remainder of the following number is: " + remainder);
+		sc.close();
 	}
 }
