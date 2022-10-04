3f06cc8fa19624e227bb78afc53e6cd6045c0b6c
Gaetano <Gaetano@gaetano>
2022-10-04 20:36:54 +0200

    Exercise 8 unfinished...

--- Exercises Topic 1/src/exercises/Exercise8.java
+++ Exercises Topic 1/src/exercises/Exercise8.java
@@ -1,8 +1,18 @@
 package exercises;
+import java.lang.*;
 import java.util.*;
 
 public class Exercise8 {
 	public static void main(String[] args) {
+		final double INFANTPRICE = 15.50;
+		final double ADULTPRICE = 20;
+		double discount = 0.05 * 100;
+		int kids, adults;
 		Scanner sc = new Scanner(System.in);
+		System.out.println("");
 	}
 }
+/*Estube teniendo otros problemas con la configuracion de Java o
+  el IDE Eclipse como las librerias. Ej: System cannot be resolved.
+                                         Scanner cannot be resolved to a type.*/
+ 
